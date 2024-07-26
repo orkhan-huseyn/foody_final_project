22cd46422b04fe7494c000942413404bc5135f7a
IbrahimAkberov <ibrahim.ekberov.2001@gamil.com>
2024-07-26 17:21:31 +0400

    i use fontawosme  and change svg  give normal responsive &, finish pls back and say someting

--- components/Footer.js
+++ components/Footer.js
@@ -1,60 +1,66 @@
 import React from 'react';
 import styles from '../styles/Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContainer}>
-        <div className={styles.footerLeft}>
-          <h1>Foody<span className={styles.dot}>.</span></h1>
-          
-          <p>Lorem ipsum is placeholder text <br />  commonly used in the graphic,</p>
-          <div className={styles.socialIcons}>
-            <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0.5 25C0.5 11.469 11.469 0.5 25 0.5C38.531 0.5 49.5 11.469 49.5 25C49.5 38.531 38.531 49.5 25 49.5C11.469 49.5 0.5 38.531 0.5 25Z" stroke="white"  />
-            
-            </svg>
-            <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25Z" fill="#FB9300" />
-            </svg>
-            <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0.5 25C0.5 11.469 11.469 0.5 25 0.5C38.531 0.5 49.5 11.469 49.5 25C49.5 38.531 38.531 49.5 25 49.5C11.469 49.5 0.5 38.531 0.5 25Z" stroke="white"/>
-            </svg>
+      <div className={styles.footerContent}>
+        <div className={styles.socialMedia}>
+          <h2>Foody<span className={styles.dot}>.</span></h2>
+           <p>  Lorem ipsum is placeholder   <br /> text commonly used in the graphic,</p>
+          <div className={styles.icons}>
+            <div className={styles.icon}>
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </div>
+            <div className={styles.icon}>
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </div>
+            <div className={styles.icon}>
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </div>
           </div>
         </div>
-        <div className={styles.footerRight}>
-          <div className={styles.footerSection}>
-            <h2>Popular</h2>
+        <div className={styles.footerLinks}>
+          <div className={styles.column}>
+            <h3>Popular</h3>
             <ul>
-              <li>Programming</li>
-              <li>Books for children</li>
-              <li>Psychology</li>
-              <li>Business</li>
+              <li><a href="/">Programming
+              </a></li>
+              <li><a href="/"> Books for children</a></li>
+              <li><a href="/">Psychology
+              </a></li>
+              <li><a href="/">Business </a></li>
             </ul>
           </div>
-          <div className={styles.footerSection}>
-            <h2>Cash</h2>
+          <div className={styles.column}>
+            <h3>Cash</h3>
             <ul>
-              <li>Delivery</li>
-              <li>Payment</li>
-              <li>About the store</li>
+              <li><a href="/">Delivery
+              </a></li>
+              <li><a href="/"> Payment
+              </a></li>
+              <li><a href="/">About the store</a></li>
+             
             </ul>
           </div>
-          <div className={styles.footerSection}>
-            <h2>Help</h2>
+          <div className={styles.column}>
+            <h3>Help</h3>
             <ul>
-              <li>Contacts</li>
-              <li>Purchase returns</li>
-              <li>Buyer help</li>
+              <li><a href="/">Contacts
+              </a></li>
+              <li><a href="/">Purchase returns
+              </a></li>
+              <li><a href="/">Buyer help
+              </a></li>
             </ul>
           </div>
         </div>
       </div>
-      <div className={styles.footerBottom}>
-        <p>All rights reserved © 2003-2022 Foody TERMS OF USE | Privacy Policy</p>
-      </div>
+      <p className={styles.copyright}>All rights reserved © 2003-2022 Foody TERMS OF USE | Privacy Policy <span>Foody</span></p>
     </footer>
   );
-}
+};
 
 export default Footer;
