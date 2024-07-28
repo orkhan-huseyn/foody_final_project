71fd59965fe935a9ac7e562a12b44463549e9edf
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2024-07-28 10:26:49 +0400

    Add prettierrc file, remove tsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React from 'react';
+import styles from '../styles/404.module.css';
 
-const Custom404 = () => {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
+const NotFoundPage = () => {
+    return (
+        <div
+            className={styles.background}
+            style={{
+                backgroundImage:
+                    'url("https://s3-alpha-sig.figma.com/img/83f2/5c8c/e7d4fcf4ff31a2e8afc9b203b6c6c1a1?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OoVeYhSI2KMmzqTKAootTymhtmgY0wbR2iR83qizxuo7dRr8qiLzdi5VlkTX0l0sC4RHw35suE7xhgCl5trWwGY9dekxuCUEGif8Ww2bXx7Azie9p6crUQUAUgIsUdXh2jtKcNgsSdw69pqvN6x0smoieP8cxPXQ-GyGm0p0J3~QZWabIdXZQi2B9Z79Ess4oaqCRP~9YUAyRoZEeQmAMwf83tEhYTFXu-Uen4FVxInlby05Dg62P4Dn7BdCeMPlW2H5~388wHBe-KxPrFWvEvJ-YWDVgTUfmWYnQXP9ef8mXLmDYrQu2GhYMoVVlaziHEOJxY2Ra3pQ~zWbTNuvDw__")',
+                backgroundRepeat: 'no-repeat',
+            }}
+        />
+    );
 };
 
-export default Custom404;
+export default NotFoundPage;
