508d7969f57cef0bd3768404bf2e09f0cbba0013
Rashad Hasan <rashadhasandev@gmail.com>
2024-07-31 01:41:00 +0400

    Add new foods section

--- components/NewFoods.js
+++ components/NewFoods.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import styles from '../styles/NewFoods.module.css';
+
+import chees from '../assets/images/NewFoods/chees.svg';
+import pizza from '../assets/images/NewFoods/pizza.svg';
+import kfc from '../assets/images/NewFoods/kfc.svg';
 
 const NewFoods = () => {
     return (
-        <section className={styles.container}>
+        <motion.section
+            className={styles.container}
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+        >
             <h2 className={styles.titleOne}>Our Popular Foods</h2>
             <h3 className={styles.titleTwo}>New Foods</h3>
             <p className={styles.text}>
@@ -11,12 +22,34 @@ const NewFoods = () => {
                 print, and publishing industries for previewing layouts and
                 visual mockups.
             </p>
+            <p className={styles.pr2}>
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries
+            </p>
             <div className={styles.cardContainer}>
-                <div>Card1</div>
-                <div>Card1</div>
-                <div>Card1</div>
+                <div className={styles.card}>
+                    <img src={chees.src} />
+                    <h3>Dubble Chees</h3>
+                    <p>
+                        Lorem ipsum is placeholder commonly used in the graphic{' '}
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <img src={pizza.src} />
+                    <h3>Margarita</h3>
+                    <p>
+                        Lorem ipsum is placeholder commonly used in the graphic{' '}
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <img src={kfc.src} />
+                    <h3>Twister Menu</h3>
+                    <p>
+                        Lorem ipsum is placeholder commonly used in the graphic{' '}
+                    </p>
+                </div>
             </div>
-        </section>
+        </motion.section>
     );
 };
 
