53221dce80d22d5a374dbc2d74ee67dc561bfdb8
Rashad Hasan <rashadhasandev@gmail.com>
2024-08-04 14:20:23 +0400

    login/register updated

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,22 @@
 import styles from '../styles/Login.module.css';
 
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Head from 'next/head';
 import { FaRegEye } from 'react-icons/fa';
 
 import loginImage from '../assets/images/login/client.png';
 import LoginHeader from 'components/LoginHeader';
 
 function Login() {
+    const router = useRouter();
+
     return (
         <>
             <LoginHeader />
+            <Head>
+                <title>Login</title>
+            </Head>
             <div className={styles.loginPage}>
                 <form className={styles.loginPageBody}>
                     <div className={styles.loginPageMain}>
@@ -17,7 +24,16 @@ function Login() {
                     </div>
                     <div className={styles.loginPageLogin}>
                         <div className={styles.loginPageLoginTitle}>
-                            <Link href="login">Login</Link>
+                            <Link
+                                href="login"
+                                className={
+                                    router.pathname === '/login'
+                                        ? styles.active
+                                        : ''
+                                }
+                            >
+                                Login
+                            </Link>
                             <Link href="register">Register</Link>
                         </div>
                         <div className={styles.loginPageLoginInputUser}>
--- pages/register.js
+++ pages/register.js
@@ -1,14 +1,20 @@
 import styles from '../styles/Register.module.css';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Head from 'next/head';
 import register from '../assets/images/register/register.svg';
-
 import { FaRegEye } from 'react-icons/fa';
 import LoginHeader from 'components/LoginHeader';
 
 function Register() {
+    const router = useRouter();
+
     return (
         <>
             <LoginHeader />
+            <Head>
+                <title>Register</title>
+            </Head>
             <div className={styles.container}>
                 <div className={styles.registerContainer}>
                     <div className={styles.imageContainer}>
@@ -17,46 +23,57 @@ function Register() {
                     <div className={styles.formContainer}>
                         <div className={styles.linkContainer}>
                             <Link href="/login">Login</Link>
-                            <Link href="/register">Register</Link>
+                            <Link
+                                href="register"
+                                className={
+                                    router.pathname === '/register'
+                                        ? styles.active
+                                        : ''
+                                }
+                            >
+                                Register
+                            </Link>
                         </div>
-                        {
-                            <form className={styles.form}>
-                                <div className={styles.formControl}>
-                                    <label for="username">Full name</label>
+                        <form className={styles.form}>
+                            <div className={styles.formControl}>
+                                <label htmlFor="fullname">Full name</label>
+                                <input
+                                    type="text"
+                                    id="fullname"
+                                    placeholder="fullname"
+                                />
+                            </div>
+                            <div className={styles.formControl}>
+                                <label htmlFor="username">Username</label>
+                                <input
+                                    type="text"
+                                    id="username"
+                                    placeholder="username"
+                                />
+                            </div>
+                            <div className={styles.formControl}>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    placeholder="user@mail.ru"
+                                />
+                            </div>
+                            <div className={styles.formControl}>
+                                <label htmlFor="password">Password</label>
+                                <div className={styles.inputContainer}>
                                     <input
-                                        type="text"
-                                        id="username"
-                                        placeholder="fullname"
+                                        type="password"
+                                        id="password"
+                                        placeholder="password"
                                     />
+                                    <FaRegEye className={styles.faEye} />
                                 </div>
-                                <div className={styles.formControl}>
-                                    <label for="username">Username</label>
-                                    <input
-                                        type="text"
-                                        id="username"
-                                        placeholder="username"
-                                    />
-                                </div>
-                                <div className={styles.formControl}>
-                                    <label for="email">Email</label>
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        placeholder="user@mail.ru"
-                                    />
-                                </div>
-                                <div className={styles.formControl}>
-                                    <label for="password">Password</label>
-                                    <div className={styles.inputContainer}>
-                                        <input type="password" placeholder='password'/>
-                                        <FaRegEye className={styles.faEye} />
-                                    </div>
-                                </div>
-                                <div className={styles.buttonContainer}>
-                                    <button>Register</button>
-                                </div>
-                            </form>
-                        }
+                            </div>
+                            <div className={styles.buttonContainer}>
+                                <button>Register</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
