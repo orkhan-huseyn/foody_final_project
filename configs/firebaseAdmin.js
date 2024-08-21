83e608e65ffcf14cb59cf02626242d7263a37164
IbrahimAkberov <ibrahim.ekberov.2001@gamil.com>
2024-08-21 11:36:45 +0400

    update date

--- configs/firebaseAdmin.js
+++ configs/firebaseAdmin.js
@@ -1,10 +1,14 @@
 import admin from 'firebase-admin';
 import { applicationDefault } from 'firebase-admin/app';
 
-admin.initializeApp({
-    credential: applicationDefault(),
-    projectId: 'foody-app-fe87a',
-});
+
+if (admin.apps.length === 0) {
+    admin.initializeApp({
+        credential: applicationDefault(),
+        projectId: 'foody-app-fe87a',
+    });
+    
+  }
 
 export const storage = admin.storage();
 export const firestore = admin.firestore();
