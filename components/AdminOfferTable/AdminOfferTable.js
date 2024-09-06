035fb8cd1739c63fa3c599276aa3ce31ed7a69e5
Rashad Hasan <rashadhasandev@gmail.com>
2024-09-06 12:23:03 +0400

    Offer section completed, request handling added.

--- components/AdminOfferTable/AdminOfferTable.js
+++ components/AdminOfferTable/AdminOfferTable.js
@@ -1,67 +1,224 @@
 import styles from './AdminOfferTable.module.css';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
-const offers = [
-    {
-        id: 9177,
-        image:  'https://s3-alpha-sig.figma.com/img/cce6/e78a/d445d897eef4d5a9f184bc4c62d631e4?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gDQzYZkDngtZMNepORGxZTuy5jMEgBEhdpWweh2BIV~kLo0gA9VGJP7mD6xFLheyi-JiADs8mBDoN7gYzzBOlUKhC1T5N9SN0OpDSVKgx14GHDHG4wk19enhhGzXMKFlEaJi1SJFyE~ZX9nUMeI~-KJXIEME4CIkaYqYCKjSQtEJCqJkul~kBsda3SyZ9AaYgCwUjrL0k4ux6eGdQWj~7Bhfbqb6oHBMSr~ySDJPah91YoTr3XgFM6G3EHwgFwlvdxhKNbCj4Tn6pfXfiKykvPPA5TSvjqeQVX1CHqcl7UlUDmUpm62mKtJ86bqAp7WHFWOO5dk0fccZ2DPoFT0H~Q__',
-        title: 'Do you like Pizza at Pap...',
-        description: 'Yummy this pizza but...',
-    },
-    {
-        id: 9178,
-        image:  'https://s3-alpha-sig.figma.com/img/cce6/e78a/d445d897eef4d5a9f184bc4c62d631e4?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gDQzYZkDngtZMNepORGxZTuy5jMEgBEhdpWweh2BIV~kLo0gA9VGJP7mD6xFLheyi-JiADs8mBDoN7gYzzBOlUKhC1T5N9SN0OpDSVKgx14GHDHG4wk19enhhGzXMKFlEaJi1SJFyE~ZX9nUMeI~-KJXIEME4CIkaYqYCKjSQtEJCqJkul~kBsda3SyZ9AaYgCwUjrL0k4ux6eGdQWj~7Bhfbqb6oHBMSr~ySDJPah91YoTr3XgFM6G3EHwgFwlvdxhKNbCj4Tn6pfXfiKykvPPA5TSvjqeQVX1CHqcl7UlUDmUpm62mKtJ86bqAp7WHFWOO5dk0fccZ2DPoFT0H~Q__',
-        title: 'Do you like Burgers at Bur...',
-        description: 'Delicious burger with extra cheese...',
-    },
-   
-];
+import AdminPageHeader from 'components/AdminPageHeader/AdminPageHeader';
+import AdminFormSection from 'components/AdminFormSection/AdminFormSection';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 function AdminOfferTable() {
+    const [offers, setOffers] = useState([]);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [selectedOfferImage, setSelectedOfferImage] = useState('');
+    const [selectedOfferId, setSelectedOfferId] = useState(null);
+    const [showForm, setShowForm] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleOfferSubmit = async () => {
+        const data = {
+            name,
+            description,
+            img_url: selectedOfferImage,
+        };
+
+        try {
+            if (isEditing) {
+                //PUT REQUEST
+                await axios.put(
+                    `http://localhost:3000/api/offer/${selectedOfferId}`,
+                    data,
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
+            } else {
+                //POST REQUEST
+                await axios.post('http://localhost:3000/api/offer', data, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+
+        fetchOffers();
+        setShowForm(false);
+        resetForm();
+    };
+
+    // GET REQUEST
+
+    const fetchOffers = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/offer');
+            const result = response.data.result.data;
+            setOffers(result);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchOffers();
+    }, []);
+
+    // DELETE REQUEST
+
+    const handleDeleteOffer = async (id) => {
+        try {
+            await axios.delete(`http://localhost:3000/api/offer/${id}`);
+            fetchOffers();
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    // Toggle form component
+
+    const handleOpenForm = () => {
+        setShowForm(true);
+    };
+
+    const handleCloseForm = () => {
+        setShowForm(false);
+    };
+
+    const handleCancelForm = () => {
+        setShowForm(false);
+        resetForm();
+    };
+
+    const handleAddOffer = () => {
+        setIsEditing(false);
+        handleOpenForm();
+        resetForm();
+    };
+
+    const handleEditOffer = (detail) => {
+        handleOpenForm();
+        setIsEditing(true);
+        setSelectedOfferId(detail.id);
+        setName(detail.name);
+        setDescription(detail.description);
+        setSelectedOfferImage(detail.img_url);
+    };
+
+    // Reset form
+
+    const resetForm = () => {
+        setName('');
+        setDescription('');
+        setSelectedOfferImage('');
+    };
+
+    const OfferFields = {
+        title: isEditing ? 'Edit Offer' : 'Add Offer',
+        informationTitle: isEditing
+            ? 'Edit your Offer information'
+            : 'Add your Offer information',
+        handleSubmit: handleOfferSubmit,
+        selectedImage: selectedOfferImage,
+        setSelectedImage: setSelectedOfferImage,
+        submitBtnName: isEditing ? 'Edit Offer' : 'Create Offer',
+        handleCancelForm,
+        informations: [
+            {
+                label: 'Title',
+                type: 'text',
+                value: name,
+                id: 'title',
+                placeholder: 'Do you like Pizza at Papa John’s?',
+                onChange: (e) => setName(e.target.value),
+            },
+            {
+                label: 'Description',
+                type: 'textarea',
+                value: description,
+                id: 'description',
+                placeholder: 'Yes you like pizza,Yummy',
+                onChange: (e) => setDescription(e.target.value),
+            },
+        ],
+    };
+
+    const headerDetails = {
+        headerTitle: 'Offers',
+        hasActionButton: true,
+        actionButtonName: 'Add offer',
+    };
+
     return (
-        <div className={styles.tableContainer}>
-            <table className={styles.offerTable}>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Image</th>
-                        <th>Title</th>
-                        <th>Descriptions</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {offers.map((offer) => (
-                        <tr key={offer.id}>
-                            <td>{offer.id}</td>
-                            <td>
-                                <img
-                                    src={offer.image}
-                                    alt={offer.title}
-                                    className={styles.offerImage}
-                                />
-                            </td>
-                            <td>{offer.title}</td>
-                            <td>{offer.description}</td>
-                            <td>
-                                <FaEdit className={styles.editIcon} />
-                                <FaTrashAlt className={styles.deleteIcon} />
-                            </td>
+        <>
+            <AdminPageHeader
+                headerDetails={headerDetails}
+                handleAdd={handleAddOffer}
+            />
+            <div className={styles.tableContainer}>
+                <table className={styles.offerTable}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Descriptions</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className={styles.paginationContainer}>
-                <div className={styles.paginationInfo}>
-                    <span>1 / 1</span>
-                </div>
-                <div className={styles.paginationControls}>
-                    <button className={styles.paginationButton}>‹</button>
-                    <select className={styles.pageSelector}>
-                        <option value="01">01</option>
-                    </select>
-                    <button className={styles.paginationButton}>›</button>
+                    </thead>
+                    <tbody>
+                        {offers.map((detail) => (
+                            <tr key={detail.id}>
+                                <td className={styles.offerId}>
+                                    <span>{detail.id}</span>
+                                </td>
+                                <td>
+                                    <img
+                                        src={detail.img_url}
+                                        alt={detail.title}
+                                        className={styles.offerImage}
+                                    />
+                                </td>
+                                <td>{detail.name}</td>
+                                <td>{detail.description}</td>
+                                <td>
+                                    <FaEdit
+                                        className={styles.editIcon}
+                                        onClick={() => handleEditOffer(detail)}
+                                    />
+                                    <FaTrashAlt
+                                        className={styles.deleteIcon}
+                                        onClick={() =>
+                                            handleDeleteOffer(detail.id)
+                                        }
+                                    />
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className={styles.paginationContainer}>
+                    <div className={styles.paginationInfo}>
+                        <span>1 / 1</span>
+                    </div>
+                    <div className={styles.paginationControls}>
+                        <button className={styles.paginationButton}>‹</button>
+                        <select className={styles.pageSelector}>
+                            <option value="01">01</option>
+                        </select>
+                        <button className={styles.paginationButton}>›</button>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            {showForm && (
+                <AdminFormSection
+                    handleCloseForm={handleCloseForm}
+                    fields={OfferFields}
+                />
+            )}
+        </>
     );
 }
 
